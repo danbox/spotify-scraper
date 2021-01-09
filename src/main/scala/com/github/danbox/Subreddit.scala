3915a3f70448fb523dcf15c3ea03f119719d7c2a
Dan Boxler <dan.boxler@netscout.com>
2021-01-09 16:58:36 -0700

    Change to a sequence of tuples, where the first is the artist, and the second is the song name (hopefully)

--- src/main/scala/com/github/danbox/Subreddit.scala
+++ src/main/scala/com/github/danbox/Subreddit.scala
@@ -7,13 +7,22 @@ class Subreddit(name: String) {
 
   private def uri(): Uri = uri"https://www.reddit.com/r/$name/hot/.json"
 
-  def getTitles: Either[String, Seq[String]] = {
-    val request = basicRequest.get(uri()).header("User-Agent", "spotify-scraper 0.1")
-    val backend = HttpURLConnectionBackend()
-    val response = request.send(backend)
-
-    response.body.map {
+  def getTitles: Either[String, Seq[(String, String)]] = basicRequest
+    .get(uri())
+    .header("User-Agent", "spotify-scraper 0.1")
+    .send(HttpURLConnectionBackend())
+    .body
+    .map {
       ujson.read(_)("data")("children").arr map { child => child("data")("title").str }
+    } map {
+      _ map{
+        postTitle => postTitle.substring(0, postTitle.indexOf('[')).trim
+      } map {
+        _.split("[-|—|\\--]")
+      } filter {
+        _.length == 2
+      } map {
+        case Array(f1, f2) => (f1, f2)
+      }
     } map { _.toSeq }
-  }
 }
